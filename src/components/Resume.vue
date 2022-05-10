cdc8e0c90ecf3fd34f2427037cd892b2463a2a5f
liangxiaole <xiaoleliang045@gmail.com>
2022-05-10 17:01:11 +0900

    init project

---
+++ src/components/Resume.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="resume">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Resume',
+  data () {
+    return {
+      msg: 'My Resume'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.resume {
+  color: red;
+}
+</style>
