160d575d58cb2fc254305d4075073a5359d20361
liangxiaole <xiaoleliang045@gmail.com>
2022-05-10 17:15:58 +0900

    add element ui lib

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="resume">
     <h1>{{ msg }}</h1>
+    <el-row>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
   </div>
 </template>
 
