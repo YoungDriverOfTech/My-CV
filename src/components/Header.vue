ab135ee70acc71bab92a74fed00758db2cee1da7
liangxiaole <xiaoleliang045@gmail.com>
2022-05-26 22:08:59 +0900

    finish basic info

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- header -->
+  <div class="header">
+    <div class="titleFontStyle el-icon-star-on">Liang's Online Resume</div>
+    <div class="navStyle">
+      <el-row>
+        <el-button type="primary" round>About me</el-button>
+        <el-button type="success" round>Resume</el-button>
+        <el-button type="warning" round>Experiences</el-button>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {};
+  },
+  components: {},
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.header {
+  height: 130px;
+  background-color: #4050b5;
+}
+
+.titleFontStyle {
+  color: white;
+  font-size: 1.6em;
+  font-weight: 700;
+  margin-top: 15px;
+}
+
+.navStyle {
+  /* display: flex; */
+  margin-top: 18px;
+  text-align: center;
+  color: white;
+  font-size: 1.2em;
+  font-weight: 500;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,26 @@
 <template>
   <div class="alignCenter">
     <!-- header -->
-    <div class="header">
-      <div class="titleFontStyle el-icon-star-on">Liang's Online Resume</div>
-      <div class="navStyle">
-        <el-row>
-          <el-button type="primary" round>About me</el-button>
-          <el-button type="success" round>My Resume</el-button>
-          <el-button type="warning" round>My Project Experiences</el-button>
-        </el-row>
-      </div>
-    </div>
+    <Header />
 
-    <!-- main -->
-    <div class="mainContentsStyle">
-      dfsfdsfds
-    </div>
+    <!-- BasicInfo -->
+    <BasicInfo />
   </div>
 </template>
 
 <script>
+import Header from "./Header.vue"
+import BasicInfo from "./BasicInfo.vue"
+
 export default {
   name: "Home",
   data() {
     return {};
   },
-  components: {},
+  components: {
+    Header,
+    BasicInfo
+  },
 
   mounted() {},
 
@@ -39,33 +34,4 @@ export default {
   margin: 0;
   padding: 0;
 }
-
-.header {
-  height: 130px;
-  background-color: #4050b5;
-}
-
-.titleFontStyle {
-  color: white;
-  font-size: 1.6em;
-  font-weight: 700;
-  margin-top: 15px;
-}
-
-.navStyle {
-  /* display: flex; */
-  margin-top: 18px;
-  text-align: center;
-  color: white;
-  font-size: 1.2em;
-  font-weight: 500;
-}
-
-.mainContentsStyle {
-  width: 70%;
-  border: 1px solid red;
-  margin: 1% 15%;
-  /* height: 800px; */
-}
-
 </style>
