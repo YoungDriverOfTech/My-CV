99fa6781406c23a2376276a1a50d96ef503b8ecc
liangxiaole <xiaoleliang045@gmail.com>
2022-05-27 11:35:33 +0900

    add project 2

--- src/components/ProjectExperiences.vue
+++ src/components/ProjectExperiences.vue
@@ -4,12 +4,14 @@
 
     <Experiences1 />
 
+    <Experiences2 />
   </div>
 </template>
 
 <script>
 import Header from "./common/Header.vue"
 import Experiences1 from "./projects/Experiences1.vue"
+import Experiences2 from "./projects/Experiences2.vue"
 
 export default {
   name: "ProjectExperiences",
@@ -19,6 +21,7 @@ export default {
   components: {
     Header,
     Experiences1,
+    Experiences2,
 
   },
 
---
+++ src/components/projects/Experiences2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ContentsContainer>
+      <template slot="title">
+        B2B Online shop system
+      </template>
+      <template slot="contents">
+        该项目是由于日本消费税由8%上升到10%，需要对原有的系统进行改修，要在画面上表示10%，以及轻减8%税率，并且要在服务器端，对逻辑进行对应的修改。
+        我的职责是： 设计书的修改，coding，单体测试，bug修正，QA回答
+      </template>
+    </ContentsContainer>
+  </div>
+</template>
+
+<script>
+import ContentsContainer from "../common/ContentsContainer.vue";
+
+export default {
+  name: "Experiences2",
+  data() {
+    return {};
+  },
+  components: {
+    ContentsContainer,
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
