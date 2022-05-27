91611974af3140e6b9a04e4e1a353012c82d8ca8
liangxiaole <xiaoleliang045@gmail.com>
2022-05-27 12:00:04 +0900

    add project 4

--- src/components/ProjectExperiences.vue
+++ src/components/ProjectExperiences.vue
@@ -7,6 +7,8 @@
     <Experiences2 />
 
     <Experiences3 />
+
+    <Experiences4 />
   </div>
 </template>
 
@@ -15,6 +17,7 @@ import Header from "./common/Header.vue"
 import Experiences1 from "./projects/Experiences1.vue"
 import Experiences2 from "./projects/Experiences2.vue"
 import Experiences3 from "./projects/Experiences3.vue"
+import Experiences4 from "./projects/Experiences4.vue"
 
 export default {
   name: "ProjectExperiences",
@@ -26,7 +29,7 @@ export default {
     Experiences1,
     Experiences2,
     Experiences3,
-
+    Experiences4,
   },
 
   mounted() {},
---
+++ src/components/projects/Experiences4.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ContentsContainer>
+      <template slot="title">
+        Tourism B2B Hotel Reservation System
+      </template>
+      <template slot="contents">
+        该项目是B2B旅游酒店预定项目，主要的负责商业公司的组团旅行的酒店预定。
+        我的职责是：coding，设计书改修，bug修正（可以适当提一下google地图的事儿）
+      </template>
+    </ContentsContainer>
+  </div>
+</template>
+
+<script>
+import ContentsContainer from "../common/ContentsContainer.vue";
+
+export default {
+  name: "Experiences4",
+  data() {
+    return {};
+  },
+  components: {
+    ContentsContainer,
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
