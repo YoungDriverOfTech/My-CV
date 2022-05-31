10144871372ba2a30c86aa1d418a01dafeb15ab9
liangxiaole <xiaoleliang045@gmail.com>
2022-05-31 17:36:21 +0900

    update resume project1

---
+++ src/components/common/ProjectContainer.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="parentMargin">
+    <div class="parentTitle underLine inlineBlock">
+      <slot name="projectName"></slot>
+    </div>
+    <div>
+      <span class="subTitle">
+        <slot name="projectDescLabel"></slot> : 
+      </span>
+      <slot name="projectDescDetail"></slot>
+
+      <br>
+      <span class="subTitle">
+        <slot name="myDutyLabel"></slot> : 
+      </span>
+      <br>
+      <slot name="myDutyDetail"></slot>
+
+      <span class="subTitle"> 
+        <slot name="techStackLabel"></slot> : 
+      </span>
+      <br>
+      <slot name="techStackDetail"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectContainer",
+  data() {
+    return {};
+  },
+  components: {},
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.parentMargin {
+  margin: 0 0 0 5%;
+}
+
+.parentTitle {
+  font-size: 1.6em;
+  font-weight: 700;
+}
+
+.underLine {
+  border-bottom: 2px solid black;
+}
+
+.inlineBlock {
+  display: inline-block;
+}
+
+.subTitle {
+  font-size: 1.2em;
+  font-weight: 600;
+}
+</style>
--- src/components/resume/Resume.vue
+++ src/components/resume/Resume.vue
@@ -5,18 +5,20 @@
         <div>
           <span class="nameFont">{{ $t("message.nameValue") }}</span>
         </div>
-        
+
         <p class="basicInfoFlexLayout">
-          <img src="../../assets/images/phone.png">
-          <span class="basicInfoBottomMargin">090-0000-0000</span>&nbsp;&nbsp;&nbsp;
-          
-          <img src="../../assets/images/email.png">
+          <img src="../../assets/images/phone.png" />
+          <span class="basicInfoBottomMargin">090-0000-0000</span
+          >&nbsp;&nbsp;&nbsp;
+
+          <img src="../../assets/images/email.png" />
           <span>xiaoleliang045@gmail.com</span>&nbsp;&nbsp;&nbsp;
 
-          <img src="../../assets/images/location.png">
-          <span>{{ $t("message.locationValue") }}</span>&nbsp;&nbsp;&nbsp;
+          <img src="../../assets/images/location.png" />
+          <span>{{ $t("message.locationValue") }}</span
+          >&nbsp;&nbsp;&nbsp;
 
-          <img src="../../assets/images/github.png">
+          <img src="../../assets/images/github.png" />
           <span>YoungDriverOfTech</span>
         </p>
       </header>
@@ -27,25 +29,30 @@
         <!-- basic information -->
         <div class="inlineBlock textLeft">
           <div>
-            <div class="el-icon-user-solid parentTitle underLine">{{ $t("message.personalInfo") }}</div>
-            <br><br>
+            <div class="el-icon-user-solid parentTitle underLine">
+              {{ $t("message.personalInfo") }}
+            </div>
+            <br /><br />
 
             <div class="subTitle">{{ $t("message.currentStatusLabel") }}</div>
-            <div class="marginOfItem">{{ $t("message.currentStatusValue") }}</div>
+            <div class="marginOfItem">
+              {{ $t("message.currentStatusValue") }}
+            </div>
 
             <div class="subTitle">{{ $t("message.blogLabel") }}</div>
             <div class="marginOfItem">https://www.yuque.com/lexiao-1kmgg</div>
 
             <div class="subTitle">Work Experiences</div>
             <div class="marginOfItem">4 years(2019-present)</div>
-
           </div>
           <br />
 
           <!-- language -->
           <div>
-            <div class="el-icon-info parentTitle underLine">{{ $t("message.chooseLanguage") }}</div>
-            <br><br>
+            <div class="el-icon-info parentTitle underLine">
+              {{ $t("message.chooseLanguage") }}
+            </div>
+            <br /><br />
 
             <div class="subTitle">{{ $t("message.english") }}</div>
             <div class="marginOfItem">{{ $t("message.toeic") }}:820</div>
@@ -63,32 +70,42 @@
 
           <!-- skill -->
           <div>
-            <div class="el-icon-s-opportunity parentTitle underLine">{{ $t("message.skills") }}</div>
-            <br><br>
+            <div class="el-icon-s-opportunity parentTitle underLine">
+              {{ $t("message.skills") }}
+            </div>
+            <br /><br />
             <!-- <div id="skill-area" style="width: `160px; height: 200px"></div> -->
 
             <div class="subTitle">Programming Languages</div>
-            <div class="marginOfItem">Java, JavaScript, TypeScript, HTML, CSS, SQL</div>
+            <div class="marginOfItem">
+              Java, JavaScript, TypeScript, HTML, CSS, SQL
+            </div>
 
             <div class="subTitle">Framework</div>
-            <div class="marginOfItem">Spring, Springboot, Mybatis, Vue, React</div>
+            <div class="marginOfItem">
+              Spring, Springboot, Mybatis, Vue, React
+            </div>
 
             <div class="subTitle">Software</div>
             <div class="marginOfItem">Git, SVN, Linux, Vim, Node.js</div>
 
             <div class="subTitle">Database</div>
-            <div class="marginOfItem">Mysql, Oracle, Postgre, SqlServer, Hirdb</div>
+            <div class="marginOfItem">
+              Mysql, Oracle, Postgre, SqlServer, Hirdb
+            </div>
           </div>
         </div>
 
         <!-- experiences and education -->
         <div class="inlineBlock textLeft">
           <!-- experience -->
-          <div class="el-icon-s-data parentTitle underLine">{{ $t("message.workExperiences") }}</div>
+          <div class="el-icon-s-data parentTitle underLine">
+            {{ $t("message.workExperiences") }}
+          </div>
           <div>
             <h3>
               <span>株式会社Q&F (2021/4 - current)</span>
-              <br>
+              <br />
               <span>Fullstack Software Engineer</span>
             </h3>
             <ul>
@@ -129,9 +146,9 @@
             <div class="el-icon-s-order parentTitle underLine">Education</div>
             <h3>
               <span>Bachelor: Financial Management</span>
-              <br>
+              <br />
               <span>Period: 2013 - 2017</span>
-              <br>
+              <br />
               <span>University: YunCheng University</span>
             </h3>
           </div>
@@ -140,57 +157,31 @@
         <div class="inlineBlock"></div>
       </div>
     </article>
+
+    <!-- projects -->
+    <article>
+      <div class="textLeft">
+        <!-- <ProjectContainer></ProjectContainer> -->
+        <ResumeProject1> </ResumeProject1>
+      </div>
+    </article>
   </div>
 </template>
 
 <script>
-import * as echarts from "echarts";
+import ProjectContainer from "../common/ProjectContainer.vue";
+import ResumeProject1 from "../resumeprojects/Project1.vue"
 
 export default {
   name: "Resume",
   data() {
     return {};
   },
-  components: {},
+  components: { ProjectContainer, ResumeProject1},
 
-  mounted() {
-    // init skill map
-    let chartDom = document.getElementById("skill-area");
-    let myChart = echarts.init(chartDom);
-    let option;
+  mounted() {},
 
-    option = {
-      radar: {
-        indicator: [
-          { name: "Java", max: 100 },
-          { name: "Spring", max: 100 },
-          { name: "JS", max: 100 },
-          { name: "CSS", max: 100 },
-          { name: "HTML", max: 100 },
-          { name: "Vue", max: 100 },
-          { name: "DB", max: 100 },
-          { name: "React", max: 100 },
-        ],
-      },
-      series: [
-        {
-          name: "skill level",
-          type: "radar",
-          data: [
-            {
-              value: [90, 90, 80, 70, 90, 80, 80, 80],
-              name: "skill score",
-            },
-          ],
-        },
-      ],
-    };
-    option && myChart.setOption(option);
-  },
-
-  methods: {
-    initRadarMap() {},
-  },
+  methods: {},
 };
 </script>
 
@@ -261,7 +252,7 @@ img {
   justify-content: center;
 }
 
-.headerStyle{
+.headerStyle {
   margin-bottom: 50px;
 }
 </style>
