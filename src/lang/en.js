2b4c6b0a79386923fc08a44be380ef58ba093120
liangxiaole <xiaoleliang045@gmail.com>
2022-06-05 14:23:24 +0900

    update words

--- src/lang/en.js
+++ src/lang/en.js
@@ -136,51 +136,51 @@ const messages = {
   // project 1
   project1Name: "Window Batch System",
   project1Period: "2019/2 - 2019/4",
-  project1Description: "This system dealing with the logs from the other servers, deleting unnecessary parts and formating these logs, sending them to another server.",
-  project1Duty: ["Communicating with customers and confirming specification", "Writing design and test ducuments", "Explanation specification for coding team", "Presentation for customers"],
+  project1Description: "This system deals with the logs from the other servers, deleting unnecessary parts and formating these logs, sending them to another server.",
+  project1Duty: ["Communicating with customers and confirming specification", "Writing design and test documents", "Explaining specification for coding team", "Presentation for customers"],
   project1TechStack: "Window Batch, Excel, Senjun",
 
   // project 2
   project2Name: "Food Online Shop",
   project2Period: "2019/5 - 2019/9",
   project2Description: "This is a B2B food online shop system that needed modification to meet the sale tax changing from 8% to 10% in 2019.",
-  project2Duty: ["Modifying existed pages and server side logic", "Writing test ducuments and doing unit test", "Fixing bugs and answering QA", "Presentation for customers"],
-  project2TechStack: "Java, VB.Net, Tomcat, Linux, HirDB, SVN, FTP",
+  project2Duty: ["Modifying existing pages and server side logic", "Writing test documents and doing unit test", "Fixing bugs and answering QA", "Presentation for customers"],
+  project2TechStack: "Java, VB.NET, Tomcat, Linux, HiRDB, SVN, FTP",
 
   // project 3
   project3Name: "Game Encrypt Check Platform",
   project3Period: "2019/10 - 2020/3",
-  project3Description: "This platform system is used to check whether if a game is encrypted properly. All the checks need to be done before this game published.",
-  project3Duty: ["Developing Restful API interfaces", "Writing Junit tests", "Creating application designs", "Reviewing source code", "Shareing thoughts in the study group"],
-  project3TechStack: "Java8, Mysql, Git/Github, Aws, Neuron, Ubuntu, Docker",
+  project3Description: "This platform system is used to check whether if a game is encrypted properly. All the checks need to be done before the game is published.",
+  project3Duty: ["Developing RESTful API", "Writing Junit tests", "Creating application designs", "Reviewing source code", "Sharing thoughts in the study group"],
+  project3TechStack: "Java8, MySQL, Git/Github, AWS, Neuron, Ubuntu, Docker, Jira",
 
   // project 4
   project4Name: "Overseas Trip Hotel Reservation Platform",
   project4Period: "2020/7 - 2020/9",
-  project4Description: "The tourism companys can use this platform to reserve hotels, customize trip courses and bug airplane ticket.",
-  project4Duty: ["Creating new pages and writing server side code", "Writing unit test design and testing", "Importing Google map into current system"],
-  project4TechStack: "Java8, Oracle, Jsp, SVN, JavaScript, Struts1, GoogleMap",
+  project4Description: "The tourism companies can use this platform to reserve hotels, customize trip courses and buy airplane tickets.",
+  project4Duty: ["Creating new pages and writing server side code", "Writing unit test design and testing", "Importing Google Maps into current system"],
+  project4TechStack: "Java8, Oracle, JSP, SVN, JavaScript, Struts1, Google Maps",
 
   // project 5
   project5Name: "Bank Transactions Monitoring System",
   project5Period: "2020/10 - 2020/11",
-  project5Description: "This system monitors bank users' transactions, which uses different conditions to check whether if users hava suspicious transactions and inserts the transaction id into a table that is used to check who is susupicous.",
+  project5Description: "This system monitors bank users' transactions, which uses different conditions to check whether if users have suspicious transactions and inserts the transaction id into a table that is used to check who is suspicious.",
   project5Duty: ["Writing application design and coding", "Fixing bugs and answering QA", "Reviewing source code and application design"],
-  project5TechStack: "Java8, SQLServer, Spring Boot, SpringBatch, Git/Github",
+  project5TechStack: "Java8, SQL Server, Spring Boot, Spring Batch, Git/Github",
 
   // project 6
   project6Name: "Online Shop Orders Management Platform",
   project6Period: "2020/12 - present",
   project6Description: "The platform consists of several parts, for example: order management, stock management, customer management, etc. I was in charge of backorder management. The main work flow of backorder management; getting order data from another server; then checking whether if orders need backorder services and updating the data status. Next, completion of data checks and finally, sending of valid data to aws S3 storage.",
   project6Duty: ["Writing application design and coding", "Creating common vue components", "Writing Junit test", "Fixing bugs and answering QA", "Reviewing source code and application design", "Releasing and deployment"],
-  project6TechStack: "Java11, Postgre, Spring Boot, SpringJPA, Git/Github, Vue.js, TypeScript, AWS, CI/CD, Flyway, Gradle",
+  project6TechStack: "Java11, PostgreSQL, Spring Boot, Spring Data JPA, Git/Github, Vue, TypeScript, AWS, CI/CD, Flyway, Gradle",
 
   // project 7
   project7Name: "Online Shop File Management System",
   project7Period: "2021/6 - present",
   project7Description: "This system is used to manipulate the files, related to business, stored in aws s3. It can upload, download files from the s3 and also can directly delete.",
   project7Duty: ["Writing application design and coding", "Creating react.js components", "Fixing bugs and answering QA", "Reviewing source code and application design", "Releasing and deployment"],
-  project7TechStack: "Java11, Postgre, Spring Boot, SpringJPA, Git/Github, React.js, TypeScript, AWS, CI/CD, Gradle, Node.js",
+  project7TechStack: "Java11, PostgreSQL, Spring Boot, Spring Data JPA, Git/Github, React, TypeScript, AWS, CI/CD, Gradle, Node.js",
 
   // ------------- projects page -------------
 }
--- src/lang/jp.js
+++ src/lang/jp.js
@@ -145,42 +145,42 @@ const messages = {
   project2Period: "2019/5 - 2019/9",
   project2Description: "2019年に消費税が10％になったと伴う、本システムに対して、画面表示内容、DBの更新方法とサーバ側のロジックを修正が必要でした。",
   project2Duty: ["既存画面の表示修正とサーバー側のロジック修正", "単体テスト設計書作成と単体テスト作業", "バグ修正とQA回答", "成果物整理"],
-  project2TechStack: "Java, VB.Net, Tomcat, Linux, HirDB, SVN, FTP",
+  project2TechStack: "Java, VB.NET, Tomcat, Linux, HiRDB, SVN, FTP",
 
   // project 3
   project3Name: "ゲーム暗号化審査システム",
   project3Period: "2019/10 - 2020/3",
   project3Description: "ゲームメーカーさん向けのシステムです。ゲームが正式にpublishまで、色々な審査を行い、審査が全部通る場合は、ゲームが販売されることが可能です。",
-  project3Duty: ["RestFul APIの開発", "Junit単体テストの作成", "設計書を作成", "ソースレビュー", "勉強会で知識や経験の共有"],
-  project3TechStack: "Java8, Mysql, Git/Github, Aws, Neuron, Ubuntu, Docker, Jira",
+  project3Duty: ["RESTful APIの開発", "Junit単体テストの作成", "設計書を作成", "ソースレビュー", "勉強会で知識や経験の共有"],
+  project3TechStack: "Java8, MySQL, Git/Github, AWS, Neuron, Ubuntu, Docker, Jira",
 
   // project 4
   project4Name: "海外旅行ホテル予約システム",
   project4Period: "2020/7 - 2020/9",
   project4Description: "旅行会社が本システムを利用して、飛行機チケットを購入、海外のホテルを予約、旅行コースをカスタマイズすることができます。",
   project4Duty: ["新規画面とサーバーサイドロジック作成", "単体テスト仕様書作成とテストの実施", "Googleマップを本システムに導入"],
-  project4TechStack: "Java8, Oracle, Jsp, SVN, JavaScript, Struts1, GoogleMap",
+  project4TechStack: "Java8, Oracle, JSP, SVN, JavaScript, Struts1, Google Maps",
 
   // project 5
   project5Name: "銀行取引監視システム",
   project5Period: "2020/10 - 2020/11",
   project5Description: "SpringBatchで銀行の取引をチェックして、もしあるユーザーは疑わしい取引がやったら、該当取引IDを抽出して、別のテーブルに登録する。.",
   project5Duty: ["設計書作成とコーディング", "バグ修正とQA回答", "ソースと設計書のレビュー"],
-  project5TechStack: "Java8, SQLServer, Spring Boot, SpringBatch, Git/Github",
+  project5TechStack: "Java8, SQL Server, Spring Boot, Spring Batch, Git/Github",
 
   // project 6
   project6Name: "オンラインショップオーダー管理システム",
   project6Period: "2020/12 - 現在",
   project6Description: "本システムはオーダー管理、マスタ管理、在庫管理、顧客情報管理、取り寄せ管理によって構成されています。私は取り寄せ管理機能を担当しています。取り寄せのワークフローは、オーダー情報を別のサーバーから取得して、取り寄せ必要チェックとオーダー有効チェックを行なってから、残ってたオーダー情報を別のサーバーに送ることです。",
   project6Duty: ["設計書作成と実装", "Vue.jsコンポーネントの実装", "Junit単体テスト作成", "バグ修正とQA回答", "ソースと設計書のレビュー", "リリース作業"],
-  project6TechStack: "Java11, Postgre, Spring Boot, SpringJPA, Git/Github, Vue.js, TypeScript, AWS, CI/CD, Flyway, Gradle",
+  project6TechStack: "Java11, PostgreSQL, Spring Boot, Spring Data JPA, Git/Github, Vue, TypeScript, AWS, CI/CD, Flyway, Gradle",
   
   // project 7
   project7Name: "オンラインショップフィル管理システム",
   project7Period: "2021/6 - 現在",
   project7Description: "本システムはAwsのS3に保存した業務関連のファイルを直接扱えます。例えばアップロード、ダウンロード、消すことができます。",
   project7Duty: ["設計書作成と実装", "Reactのコンポーネントの実装", "バグ修正とQAの回答", "ソースと設計書のレビュー", "リリース作業"],
-  project7TechStack: "Java11, Postgre, Spring Boot, SpringJPA, Git/Github, React.js, TypeScript, AWS, CI/CD, Gradle, Node.js",
+  project7TechStack: "Java11, PostgreSQL, Spring Boot, Spring Data JPA, Git/Github, React, TypeScript, AWS, CI/CD, Gradle, Node.js",
 
   // ------------- projects page -------------
 }
